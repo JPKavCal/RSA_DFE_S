9a20e073689023598701d2f4827627da389d2215
jpkavcal <K3rryMas0n*>
2023-03-16 22:55:03 +0100

    Expanded to regions A and C Refined map to prevent erroneous overloading of PC

--- LittleHelpers.py
+++ LittleHelpers.py
@@ -20,22 +20,8 @@ def sidebar():
 def assemble():
     if 'initiated' not in st.session_state:
         st.session_state.catch = None
-        st.session_state.catchCoord = None
         st.session_state.selreg = 0
         st.session_state.selstat = 0
-        st.session_state.selcoord = 0
-        st.session_state.selgpt = 0
-        st.session_state.catchsel = 0
-        st.session_state.runcount = 0
-        st.session_state.dws_prim = '../../Data Processing/0_External_Data/DWS SHP/primary catchments.shp'
-        st.session_state.lastclick = 0
-
-        st.session_state.closestdre = 0
-        st.session_state.grp = 0
-        st.session_state.grp_daily = 0
-        st.session_state.c_dre = 0
-        st.session_state.cluster = 0
-        st.session_state.floods = 0
 
         st.set_page_config(
             page_title="Study Stations",
--- pages/1_Study_Stations.py
+++ pages/1_Study_Stations.py
@@ -12,6 +12,7 @@ sidebar()
 
 @st.cache_data  # (allow_output_mutation=True)
 def load_stations(station_file, catch_file):
+    # Caching function for loading the regional stations into a geopandas dataframe
     stats = gp.GeoDataFrame.from_file(station_file)
     stats['lon'] = stats.long
     regc = gp.GeoDataFrame.from_file(catch_file)
@@ -21,27 +22,26 @@ def load_stations(station_file, catch_file):
 
 @st.cache_data  # (allow_output_mutation=True)
 def load_primary(catch_file):
+    # Caching function for loading the DWS Primary drainage regions into a geopandas dataframe
     regc = gp.GeoDataFrame.from_file(catch_file)
     regc.geometry = regc.simplify(0.01)
     regc.drop('COUNT', axis=1, inplace=True)
     return regc
 
 
-# primcatch_path = '../../Data Processing/0_External_Data/DWS SHP/primary catchments.shp'
-# gdf = gp.read_file(primcatch_path)
-
+# Link to google drive as test of speed
 dws_prim = 'https://drive.google.com/uc?id=1_iivRT1-Fb8wvgIPfRX2guebFD5FnlBy'
-# dws_prim = '../../Data Processing/0_External_Data/DWS SHP/primary catchments.shp'
-gdf_dws_prim = load_primary(dws_prim)
-gdf = gdf_dws_prim.copy()
+gdf = load_primary(dws_prim)
 
 st.title("Study Stations and Catchments")
 
 col1, col2 = st.columns(2)
 
+# Define list of DWS primary drainage regions, could also extract from gdf...
 reg_options = ['All', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J',
                'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X']
-sel_type = 'Station'
+# sel_type = 'Station'
+
 with col1:
     region = st.selectbox("Select Region", reg_options, index=st.session_state.selreg)
     st.session_state.selreg = reg_options.index(region)
@@ -62,21 +62,16 @@ if region != 'All':
         catch = gdf[gdf.PRIMARY == region].copy()
         st.session_state.catch = None
         st.session_state.selstat = 0
-    elif sel_type == 'Station':
+    else:  # if sel_type == 'Station':
         st.session_state.selstat = options.index(station)
         stat = stations[stations['gauge'] == station]
         catch = regcatch[regcatch['gauge'] == station].loc[:, ['gauge', 'Area', 'Lc', 'ARF', 'Tc', 'geometry']]
         catch = np.round(catch, 2)
 
         st.session_state.catch = regcatch[regcatch['gauge'] == station].drop('geometry', axis=1)
-    else:
-        # st.session_state.selstat = 0
-        st.session_state.catch = None
-        catch = gdf[gdf.PRIMARY == region].copy()
-
 
 else:
-    catch = gdf_dws_prim
+    catch = gdf
     st.session_state.selstat = 0
 
 lon, lat = leafmap.gdf_centroid(catch)
@@ -90,33 +85,45 @@ m.add_tile_layer(
     attribution="Google",
 )
 
-
-if region != 'All' and sel_type == 'Station':
+if region != 'All' and st.session_state.selstat == 0:  # and sel_type == 'Station':
     m.add_gdf(stat, layer_name='Study Stations')
+elif st.session_state.selstat != 0:
+    m.add_marker((stat.iloc[0].geometry.y, stat.iloc[0].geometry.x))
 
+height = 600
 m.add_gdf(catch, layer_name='Primary Catchment')
 m.zoom_to_gdf(catch)
 
-height = 600
-out = m.to_streamlit(height=height, bidirectional=True)
-# out
-p = out['last_object_clicked']
+if st.session_state.selstat == 0:
 
-if p is not None:
-    if region != 'All' and sel_type == 'Station':
-        cpl = stations.sindex.nearest(Point(p['lng'], p['lat']), return_distance=True)
-        cp = cpl[0][1][0]
-        dist = cpl[1][0]
-        if dist == 0:
-            st.session_state.selstat = options.index(stat.iloc[cp].gauge)
-            st.experimental_rerun()
-    elif region != 'All' and sel_type != 'Station':
-        pass
-    else:
-        cpl = catch.sindex.nearest(Point(p['lng'], p['lat']), return_distance=True)
-        cp = cpl[0][1][0]
-        try:
-            st.session_state.selreg = reg_options.index(catch.iloc[cp].PRIMARY)
-            st.experimental_rerun()
-        except ValueError:
-            pass
+    out = m.to_streamlit(height=height, bidirectional=True)
+    p = out['last_object_clicked']
+
+    if p is not None:
+        if region != 'All' and st.session_state.selstat == 0:
+            cpl = stations.sindex.nearest(Point(p['lng'], p['lat']), return_distance=True)
+            cp = cpl[0][1][0]
+            dist = cpl[1][0]
+            if dist == 0:
+                st.session_state.selstat = options.index(stat.iloc[cp].gauge)
+                st.experimental_rerun()
+        elif region == 'All':
+            cpl = catch.sindex.nearest(Point(p['lng'], p['lat']), return_distance=True)
+            cp = cpl[0][1][0]
+            try:
+                st.session_state.selreg = reg_options.index(catch.iloc[cp].PRIMARY)
+                st.experimental_rerun()
+            except ValueError:
+                st.write("Error here...")
+                pass
+
+else:
+    stcol1, stcol2 = st.columns(2)
+    with stcol1:
+        m.to_streamlit(height=height)
+
+    with stcol2:
+        stat.drop('geometry', axis=1, inplace=True)
+        catch.drop('geometry', axis=1, inplace=True)
+        st.dataframe(stat)
+        st.dataframe(catch)
